77d4843db2c730f8804bdb0bcdad81840a759d85
Dinuka <dinuka.ganganathr@gmail.com>
2023-11-18 08:09:51 -0800

    First Commit

---
+++ src/main/java/com/sampathproducts/SampathproductsApplication.java
@@ -0,0 +1,13 @@
+package com.sampathproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampathproductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampathproductsApplication.class, args);
+	}
+
+}
