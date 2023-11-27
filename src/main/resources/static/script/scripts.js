8f8992cd556af26da5a109f89fa1f301d35bf735
Dinuka <dinuka.ganganathr@gmail.com>
2023-11-27 10:14:26 +0530

    web nav bar and side bar finished

--- src/main/resources/static/script/scripts.js
+++ src/main/resources/static/script/scripts.js
@@ -1,8 +1,28 @@
-const body = document.querySelector("body"),
-      sidebar = body.querySelector(".sidebar"),
-      toggle = body.querySelector(".toggle");
+// Menu toggle with chevro button
+function menuToggle(element){
+    const menu = document.querySelector('.sidebar');
 
-    
-    toggle.addEventListener("click", () =>{
-        sidebar.classList.toggle("close");
-    });
+    if(menu.classList.contains('show')){
+        menu.classList.remove('show');
+        menu.classList.add('hide');
+        element.classList.remove('fa-circle-chevron-left');
+        element.classList.add('fa-circle-chevron-right');
+    }else{
+        menu.classList.remove('hide');
+        menu.classList.add('show');
+        element.classList.add('fa-circle-chevron-left');
+        element.classList.remove('fa-circle-chevron-right');
+    }
+}
+
+// Mobile view of side bar toggle
+function mobileToggle(element){
+    const mobile_menu = document.querySelector('.sidebar');
+    console.log(mobile_menu.classList);
+
+    if(mobile_menu.classList.contains('mob_show')){
+        mobile_menu.classList.remove('mob_show');
+    }else{
+        mobile_menu.classList.add('mob_show');
+    }
+}
