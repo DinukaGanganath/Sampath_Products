d392cba75c8953c0b14b8cfbbe8a9bc1992cc699
Dinuka <dinuka.ganganathr@gmail.com>
2024-03-11 11:27:08 +0530

    obj create 2

--- src/main/resources/static/script/jsonObjCreator.js
+++ src/main/resources/static/script/jsonObjCreator.js
@@ -4,7 +4,7 @@ function createJson(eleid, parentEle){
         var jsonAttr = document.getElementById(eleid).querySelectorAll(parentEle);
         for(attr of jsonAttr){
             if(attr.id != ""){
-                switch(attr.localName){
+                /*switch(attr.localName){
                     case "td":
                         jsonObj += `"${attr.id}":"${attr.innerHTML}",`;
                         break;
@@ -13,15 +13,17 @@ function createJson(eleid, parentEle){
                         break;
                     case "select":
                         jsonObj += `"${attr.id}": ${JSON.parse(attr.value)} ,`;
-                }
+                }*/
+
+                jsonObj += `"${attr.id}":"",`;
                 
             }
         }
     }
     
     jsonObj = jsonObj.slice(0, -1);
-   // console.log(jsonObj);
+    console.log(jsonObj);
     var obj = JSON.parse('{'+jsonObj+'}');
 
-    console.log(obj);
+    console.log(Object.keys(obj));
 }
